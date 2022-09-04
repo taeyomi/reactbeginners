4b21b65e72660266a71e6b3b0774c7f2e4dc936c
taeyomi <100909703+taeyomi@users.noreply.github.com>
2022-09-04 13:56:27 +0900

    todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import Button from "./Button";
+import {useState} from "react";
+
 function App() {
-  return (
-    <div><h1>Welcome back!</h1>
-    <Button text = {"Continue"}/>
-    </div>
+  const [toDo, setToDo]  = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    } //todo가 empty라면 함수 작동 x
+    setToDo(""); 
+    setToDos((currentArray)=>[toDo, ...currentArray]);
+};
+  return( 
+  <div>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input 
+      onChange={onChange} 
+      value={toDo} 
+      type="text" 
+      placeholder="Write your to do.."
+    />
+    <Button>Add To Do</Button>
+    </form>
+    <hr/>
+    <ul>
+    {toDos.map((item, index) =>
+    (<li key={index}>{item}</li>))}
+    </ul>
+
+  </div>
   );
 }
 
