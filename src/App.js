52be21ef8ab72b05fd1c7b9d146d498b4602580e
taeyomi <100909703+taeyomi@users.noreply.github.com>
2022-09-06 16:30:14 +0900

    cointraker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import Button from "./Button";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [toDo, setToDo]  = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    } //todo가 empty라면 함수 작동 x
-    setToDo(""); 
-    setToDos((currentArray)=>[toDo, ...currentArray]);
-};
-  return( 
-  <div>
-    <h1>My To Dos ({toDos.length})</h1>
-    <form onSubmit={onSubmit}>
-      <input 
-      onChange={onChange} 
-      value={toDo} 
-      type="text" 
-      placeholder="Write your to do.."
-    />
-    <Button>Add To Do</Button>
-    </form>
-    <hr/>
-    <ul>
-    {toDos.map((item, index) =>
-    (<li key={index}>{item}</li>))}
-    </ul>
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]); //[]기본값을 지정해야 undefined가 되지 않음
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response=>response.json())
+    .then((json)=> {
+      setCoins(json);
+      setLoading(false);
+    });  
+  }, []);
 
+  return (<div>
+    <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+
+      
   </div>
   );
 }
